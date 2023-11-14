fde264262fb8067a32edc5dd7e816f3bc68255a7
Gowtham Subramani <gowtham@klubworks.com>
2023-11-14 08:06:53 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h3>Dashboard coming soon...</h3>
+    </main>
+  )
+}
